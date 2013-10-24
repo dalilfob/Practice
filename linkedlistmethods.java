5766880e507e00d232f3b68a36a84e25cfa4170a
dalilfob <crazietoan@gmail.com>
2013-10-24 01:13:38 -0700

    Update linkedlistmethods.java

--- linkedlistmethods.java
+++ linkedlistmethods.java
@@ -14,31 +14,30 @@ public void add_to_head(int key, double value) {
 		count++;
 	}
 	
-	
-	/**
-	 * This method will remove the last element in the list and return the value
-	 * of the last element.
-	 * 
-	 */
-	// TODO Fix the problem that current is not truly null after setting to null
-	public void remove_tail() {
-		if (head.next != null) {
-			Item current = head;
-			while (current.next != null) {
-				current = current.next;
-			}
-			current = null;
-			count--;
-		}
-	}
-
-	public void print() {
+/**
+* This method will remove the last element in the list and return the value
+* of the last element.
+* 
+*/
+// TODO Fix the problem that current is not truly null after setting to null
+public void remove_tail() {
+	if (head.next != null) {
 		Item current = head;
-		System.out.printf("%s\t%5s\n", "Node", "Value");
 		while (current.next != null) {
-			System.out.printf("%d\t%.2f\n", current.next.key,
-					current.next.value);
 			current = current.next;
 		}
+		current = null;
+		count--;
 	}
 }
+
+public void print() {
+	Item current = head;
+	System.out.printf("%s\t%5s\n", "Node", "Value");
+	while (current.next != null) {
+		System.out.printf("%d\t%.2f\n", current.next.key,
+				current.next.value);
+		current = current.next;
+	}
+}
+
